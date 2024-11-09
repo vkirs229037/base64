4d5dd36bc8211cc8c48b9381fa2cf272c2e7cdff
vkirs229037 <vova.kirsanov.03@mail.ru>
2024-11-10 02:12:03 +0300

    Тест реализации base64_encode

--- main.c
+++ main.c
@@ -1,5 +1,9 @@
 #include "stdio.h"
+#define BASE64_IMPLEMENTATION
+#include "base64.h"
 
 int main(void) {
+    char* encoded = base64_encode("Hello World!", 12);
+    printf("%s\n", encoded);
     return 0;
 }
